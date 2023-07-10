a4e393e1564c76915e25c40f65a9951f4459e5ea
yoshihiro-tada <shyuwatx@gmail.com>
2023-07-10 17:46:02 +0900

    LocationStyle調整

--- src/components/Templates/Main/Location/Location.tsx
+++ src/components/Templates/Main/Location/Location.tsx
@@ -12,16 +12,19 @@ class Location extends React.Component {
             <p className={styles.introduce}>機械場の編み機の稼働状況が一覧で確認できます。</p>
             <h3>凡例</h3>
             <div className={styles.ex_box_flex}>
-              <SquareIcon className={styles.ex_box_on} />
+              <SquareIcon color="success" fontSize="large" />
               <p>:稼働中</p>
             </div>
             <div className={styles.ex_box_flex}>
-              <SquareIcon className={styles.ex_box_off} />
+              <SquareIcon color="error" fontSize="large" />
               <p>:停止中</p>
             </div>
             <div className={styles.location_wrap_box}>
               <div className={styles.rows_flex}>
-                <SquareIcon /><SquareIcon /><SquareIcon /><SquareIcon />
+                <SquareIcon fontSize="large" />
+                <SquareIcon fontSize="large" />
+                <SquareIcon fontSize="large" />
+                <SquareIcon fontSize="large" />
               </div>
             </div>
       </React.Fragment>
