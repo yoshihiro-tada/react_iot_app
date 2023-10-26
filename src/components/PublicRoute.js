3ab34472a1406db7300356574589864e70148e37
yoshihiro-tada <shyuwatx@gmail.com>
2023-10-26 17:31:22 +0900

    PublicRoute.jsファイルの作成

---
+++ src/components/PublicRoute.js
@@ -0,0 +1,14 @@
+import { Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
+
+const PublicRoute = ({ component: Component, ...rest }) => {
+  const { user } = useAuthContext();
+  return (
+    <Route
+      {...rest}
+      render={(routeProps) => {
+        return !user ? <Component {...routeProps} /> : <Navigate to="/" />;
+      }}
+    />
+  );
+};
