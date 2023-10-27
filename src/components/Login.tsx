2df51abfa4387bd01c34c75b010e566a42aa729f
yoshihiro-tada <shyuwatx@gmail.com>
2023-10-27 18:50:22 +0900

    ログイン失敗時にエラー文をブラウザ表示-修正

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,29 +1,44 @@
 import { auth } from '../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import { Link, useNavigate } from 'react-router-dom';
 import styles from './Login.module.css';
 import { useState } from 'react';
 
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [error, setError] = useState('');
-  const handleSubmit = (event:any) => {
+  const handleSubmit = async (event:any) => {
     event.preventDefault();
     const { email, password } = event.target.elements;
-    {/* await ←非同期処理の完了を待ちたいが、何故かエラーが出る */} 
-    try {
-      auth.signInWithEmailAndPassword(email.value, password.value);
+    signInWithEmailAndPassword(auth, email.value, password.value).then(() => {
       navigate('/');
-    } catch(error:any) {
-      console.log(error);
-      setError(error.message);
-    }
-  };
+    })
+    .catch((error:any) => {
+      switch (error.code) {
+        case 'auth/invalid-email':
+          setError('正しいメールアドレスの形式で入力してください。');
+          break;
+        case 'auth/user-not-found':
+          setError('メールアドレスかパスワードに誤りがあります。');
+          break;
+        case 'auth/wrong-password':
+            setError('メールアドレスかパスワードに誤りがあります。');
+            break;
+        default:
+          setError('メールアドレスかパスワードに誤りがあります。');
+          break;
+      }
+    });
+  }
 
   return (
     <>
       <h2>ログイン</h2>
+      {/*
       {error && <p style={{ color:'red' }}>{error}</p>}
+      */}
       <form className={styles.login_form} onSubmit={handleSubmit}>
+        {error && <p style={{ color:'red' }}>{error}</p>}
         <div>
           <label>メールアドレス</label>
           <input name="email" type="email" placeholder="email" />
@@ -42,7 +57,6 @@ const Login = () => {
       <div className={styles.signup_link}>※ユーザー登録は<Link to={'/signup'}>こちら</Link>から</div>
     </>
   );
-
 };
 
 export default Login;
