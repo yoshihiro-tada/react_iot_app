c2a118259691bb2d2a8002dd46825fbd7c220b6a
yoshihiro-tada <shyuwatx@gmail.com>
2023-11-18 13:49:54 +0900

    firebase.js > コメントアウトとconsole.logの削除

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-{/*
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-*/}
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -13,8 +9,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
 };
-
-console.log(firebaseConfig)
 
 initializeApp(firebaseConfig);
 
